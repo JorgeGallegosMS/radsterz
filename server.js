62857e03996f8b97def0548b5176dc4a0ef23e2a
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-08-24 23:21:52 -0700

    Setup graphql

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
+if (process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
 const express = require('express')
-
+const { graphqlHTTP } = require('express-graphql')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQuery',
+  description: 'The root query',
+  fields: () => ({
+    hello: {
+      type: GraphQLString,
+      description: 'Shows hello',
+      resolve: () => 'Hello'
+    }
+  })
+})
 
-app.listen(port, () => console.log(`Server started at http://localhost:${port}`))
+const schema = new GraphQLSchema({
+  query: RootQueryType
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started at http://localhost:${port}/graphql`))
